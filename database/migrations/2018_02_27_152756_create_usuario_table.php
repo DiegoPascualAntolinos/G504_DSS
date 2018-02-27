e92ad8bc371fca3c85b5f89ac2dba9a1ec914254
Javier Espí <jec22@alu.ua.es>
2018-02-27 16:38:04 +0100

    Creacion de la tabla Usuario

---
+++ database/migrations/2018_02_27_152756_create_usuario_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsuarioTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('usuario', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('DNI');
+            $table->string('Nombre');
+            $table->string('Direccion');
+            $table->string('Email');
+            $table->string('Telefono');
+            $table->string('Tipo');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('usuario');
+    }
+}
