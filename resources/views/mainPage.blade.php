58dd614a90d3252a78a5ca94a25dcc0de89a5cab
Diego <dpa21@alu.ua.es>
2018-04-10 08:26:23 -0700

    Arreglar perfil

--- resources/views/mainPage.blade.php
+++ resources/views/mainPage.blade.php
@@ -39,7 +39,12 @@
     <span class="sr-only">Next</span>
   </a>
 </div>
-
+<div class="jumbotron jumbotron-fluid">
+  <div class="container">
+    <h1 class="display-4">Fluid jumbotron</h1>
+    <p class="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
+  </div>
+</div>
 <div class="container">
 
   <div class="row align=center">
