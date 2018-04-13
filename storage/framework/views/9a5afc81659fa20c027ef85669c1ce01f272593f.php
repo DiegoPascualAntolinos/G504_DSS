989a618e3e8d393955aa64f051648e42595eb38b
Andrea Soler Pla <asp106@alu.ua.es>
2018-04-13 11:07:17 +0200

    Contacto Dsstinos terminado

--- storage/framework/views/9a5afc81659fa20c027ef85669c1ce01f272593f.php
+++ storage/framework/views/9a5afc81659fa20c027ef85669c1ce01f272593f.php
@@ -5,83 +5,61 @@
 
 <?php $__env->startSection('content'); ?>
 
-<div class="sidebar-widget">
-            <!-- Sidebar Contact info -->
-            <div class="styled-box gray">
-              <h3 class="text-upper">Contacta con nosotros</h3>
-              <p>Somos especialistas en hacerte feliz.</p>
-              <ul class="contact-info list-unstyled">
-                <li class="ct-phone">+34 93 409 50 03</li>
-                <li class="ct-email">viajerosUA@DSStinos.com</li>
-                <li class="ct-facebook"><a href="https://www.facebook.com/" target="_blank">www.facebook.com</a></li>
-               
-              </ul>
+
+    <div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="well well-sm">
+                <form class="form-horizontal" method="post">
+                    <fieldset>
+                        <legend class="text-center header">Explícanos qué necesitas</legend>
+
+                        <div class="form-group">
+                            <span class="col-md-1 col-md-offset-2 text-center"><i class="fa fa-user bigicon"></i></span>
+                            <div class="col-md-8">
+                                <input id="fname" name="name" type="text" placeholder="Nombre" class="form-control">
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <span class="col-md-1 col-md-offset-2 text-center"><i class="fa fa-user bigicon"></i></span>
+                            <div class="col-md-8">
+                                <input id="lname" name="name" type="text" placeholder="Apellidos" class="form-control">
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <span class="col-md-1 col-md-offset-2 text-center"><i class="fa fa-envelope-o bigicon"></i></span>
+                            <div class="col-md-8">
+                                <input id="email" name="email" type="text" placeholder="Email Address" class="form-control">
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <span class="col-md-1 col-md-offset-2 text-center"><i class="fa fa-phone-square bigicon"></i></span>
+                            <div class="col-md-8">
+                                <input id="phone" name="phone" type="text" placeholder="Teléfono" class="form-control">
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <span class="col-md-1 col-md-offset-2 text-center"><i class="fa fa-pencil-square-o bigicon"></i></span>
+                            <div class="col-md-8">
+                                <textarea class="form-control" id="message" name="message" placeholder="Escribe el mensaje correspondiente." rows="7"></textarea>
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <div class="col-md-12 text-center">
+                                <button type="enviar" class="btn btn-primary btn-lg">Enviar</button>
+                            </div>
+                        </div>
+                    </fieldset>
+                </form>
             </div>
-          </div>
-
-          
-<div class= "container">
-    <div class="container container-centered">
-<h1>Explícanos qué necesitas</h1>
-</div> 
-<div class="column size-1of4">
-
-<form name="frmContacto" method="POST">
-<?php echo csrf_field(); ?>
-
-<table width="500px">
-<tr>
-<td>
-<label for="first_name">Nombre: *</label>
-</td>
-<td>
-<input type="text" name="first_name" maxlength="50" size="25">
-</td>
-</tr>
-<tr>
-<td valign="top">
-<label for="last_name">Apellidos: *</label>
-</td>
-<td>
-<input type="text" name="last_name" maxlength="50" size="25">
-</td>
-</tr>
-<tr>
-<td>
-<label for="email">Email: *</label>
-</td>
-<td>
-<input type="text" name="email" maxlength="80" size="35">
-</td>
-</tr>
-<tr>
-<td>
-<label for="telephone">Teléfono contacto:</label>
-</td>
-<td>
-<input type="text" name="telephone" maxlength="25" size="15">
-</td>
-</tr>
-<tr>
-<td>
-<label for="comments">Comentario: *</label>
-</td>
-<td>
-<textarea name="comments" maxlength="500" cols="30" rows="5"></textarea>
-</td>
-</tr>
-<tr>
-<td colspan="2" style="text-align:right">
-    <div class="col-xs-2">
- <div class="row row-centered">
-        <button type="sumbit">Enviar</button>
+        </div>
     </div>
 </div>
-</td>
-</tr>
-</table>
-</form>
-</div>
+
 <div class="column size-1of4">
     <iframe src="" width="200" height="100" frameborder="0" style="border:0" allowfullscreen></iframe>
 </div>
