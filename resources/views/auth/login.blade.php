a2b69552f80a106a979fb803f03adb85d2ea4e18
Javier Espi <jec22@alu.ua.es>
2018-05-22 13:37:12 +0200

    arreglado login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app')
+@extends('layout.mainlayout')
 <!-- El [arroba]extends sirve para que automaticamente ponga la cabecera --> 
 <!--el footer y todos los estilos en las paginas. Es como cargar -->
 <!--la pagina maestra -->
